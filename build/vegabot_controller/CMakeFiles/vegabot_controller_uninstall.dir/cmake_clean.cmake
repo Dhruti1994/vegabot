879faeca39d5f5b0a77179b39c403fb1c912618d
Dhruti1994 <dhruti1357@gmail.com>
2026-09-13 12:13:10 +0530

    added gitignore

--- build/vegabot_controller/CMakeFiles/vegabot_controller_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vegabot_controller_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vegabot_controller_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
